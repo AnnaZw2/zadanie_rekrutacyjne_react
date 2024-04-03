1329256ee2efef691b9f252ccae2d1daddc59948
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2024-04-03 19:16:48 +0200

    add sorting

--- my-app/src/components/TagList.tsx
+++ my-app/src/components/TagList.tsx
@@ -8,8 +8,10 @@ import {
   Table,
   TableBody,
   TableCell,
+  TableContainer,
   TableHead,
   TableRow,
+  TableSortLabel,
   TextField,
 } from "@mui/material";
 
@@ -20,10 +22,13 @@ function TagList() {
   const [error, setError] = useState<Error | null>(null);
   const [warning, setWarning] = useState<string | null>(null);
   const [loading, setLoading] = useState(true);
+  const [sortBy, setSortBy] = useState<"name" | "popular">("popular");
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
   const defaultVal = 25;
   const minVal = 10;
   const maxVal = 200;
   const [value, setValue] = useState<number>(defaultVal);
+
   useEffect(() => {
     // checking loader
     // setTimeout(() => {
@@ -33,10 +38,14 @@ function TagList() {
     getTags();
   }, []);
 
-  async function getTags(elementsPerPage = defaultVal) {
+  async function getTags(
+    elementsPerPage = defaultVal,
+    sortFiled = "popular",
+    sortDirection = "desc"
+  ) {
     try {
       const response = await fetch(
-        `https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&site=stackoverflow&key=4UBaAyJbbK0NiBol8i*vZA((&pagesize=${elementsPerPage}`
+        `https://api.stackexchange.com/2.3/tags?order=${sortDirection}&sort=${sortFiled}&site=stackoverflow&key=4UBaAyJbbK0NiBol8i*vZA((&pagesize=${elementsPerPage}`
       );
       const data = await response.json();
       const fomrmatData: Tag[] = data.items.map((item: any) => {
@@ -90,15 +99,17 @@ function TagList() {
     setWarning(null);
   };
 
-  const columns = [
-    { field: "name", headerName: "Name of tag", flex: 1 },
-    {
-      field: "count",
-      headerName: "Number of related posts",
-      flex: 1,
-      valueGetter: (params: any) => params.row.count.toLocaleString(),
-    },
-  ];
+  const handleSort = (field: "name" | "popular") => {
+    if (sortBy === field) {
+      const newSortOrder = sortOrder === "asc" ? "desc" : "asc";
+      setSortOrder(newSortOrder);
+    } else {
+      // Reset sort order
+      setSortOrder("asc");
+      setSortBy(field);
+    }
+    getTags(value, field, sortOrder === "asc" ? "desc" : "asc");
+  };
 
   return (
     <div className="container">
@@ -114,7 +125,6 @@ function TagList() {
               min: minVal,
               max: maxVal,
             }}
-            style={{ width: "8vh"}}
             size="small"
             value={value}
             onChange={handleChange}
@@ -134,28 +144,42 @@ function TagList() {
         </Alert>
       )}
 
-      {/* <ul className="tags-container">
-        <li>Name of the tag</li>
-        <li>Number of related posts</li>
-        {tags.map((tag: Tag) => (
-          <TagElement tag={tag} />
-        ))}
-        {}
-      </ul> */}
+      <TableContainer sx={{ maxWidth: "100%", overflowX: "auto" }}>
+        <Table
+          sx={{
+            width: { xs: "100%", sm: "80vh" },
+            margin: "auto",
+            marginBottom: "10vh",
+          }}
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ fontWeight: "bold" }}>
+                Name of the tag
+                <TableSortLabel
+                  active={sortBy === "name"}
+                  direction={sortOrder}
+                  onClick={() => handleSort("name")}
+                />
+              </TableCell>
 
-      <Table sx={{ width: "80vh", margin: "auto",marginBottom:"10vh"  }}>
-        <TableHead>
-          <TableRow>
-            <TableCell sx={{fontWeight:"bold"}}>Name of the tag</TableCell>
-            <TableCell sx={{fontWeight:"bold"}}>Number of related posts</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {tags.map((tag: Tag) => (
-            <TagElement key={tag.name} tag={tag} />
-          ))}
-        </TableBody>
-      </Table>
+              <TableCell sx={{ fontWeight: "bold" }}>
+                Number of related posts
+                <TableSortLabel
+                  active={sortBy === "popular"}
+                  direction={sortOrder}
+                  onClick={() => handleSort("popular")}
+                />
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {tags.map((tag: Tag) => (
+              <TagElement key={tag.name} tag={tag} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
