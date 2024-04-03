8d054db440cd2f6a4a20fdc62318159bcd849890
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2024-04-03 17:34:57 +0200

    reformat file

--- my-app/src/components/TagElement.tsx
+++ my-app/src/components/TagElement.tsx
@@ -1,20 +1,19 @@
 import { Tag } from "../types";
-import { ListItem,ListItemText } from '@mui/material';
+import { Divider, ListItem, ListItemText } from "@mui/material";
 
 interface TagElementProps {
   tag: Tag;
 }
 
 function TagElement({ tag }: TagElementProps) {
-    const formatCount = tag.count.toLocaleString();
+  const formatCount = tag.count.toLocaleString();
   return (
-
-    <ListItem>
-    <ListItemText
-      primary={tag.name}
-      secondary={`Related posts: ${formatCount}`}
-    />
-  </ListItem>
+    <ListItem >
+      <ListItemText
+        primary={tag.name}
+        secondary={`Related posts: ${formatCount}`}
+      />
+    </ListItem>
   );
 }
 export { TagElement };
